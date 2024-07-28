dff5fcd647db29b5344da824231b1fa331903388
Ahmadreza <ahmadrezareyhani@aryehco.com>
2024-07-28 15:51:56 +0330

    + add InMem UserDetailsService

--- src/main/java/ir/reyhani/security/configs/SecurityConfig.java
+++ src/main/java/ir/reyhani/security/configs/SecurityConfig.java
@@ -2,9 +2,16 @@ package ir.reyhani.security.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -20,11 +27,28 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/index.html", "/assets/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/index.html", "/assets/**").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.builder()
+                        .username("ahmadreza")
+                        .password(passwordEncoder().encode("password"))
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
